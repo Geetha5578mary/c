35394783ca51821e173068a076ef3f73b4b468a7
Geetha5578mary <36623765+Geetha5578mary@users.noreply.github.com>
2018-05-28 13:36:20 +0530

    Update HELLO.C

--- HELLO.C
+++ HELLO.C
@@ -1,14 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int n,i;
-clrscr();
-printf("enter the number of times HELLO word want to print");
-scanf("%d",&n);
+scanf("%d",&n); 
 for(i=1;i<=n;i++)
 {
-printf("HELLO\n");
+printf("Hello\n");
 }
-getch();
 }
